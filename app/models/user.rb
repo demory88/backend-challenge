0dcb702148e65eac692ffd6d078a49422fe15e8f
Don Emory <demory88@gmail.com>
2020-12-08 15:36:46 -0600

    Tag scanning script v1

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,42 @@ class User < ApplicationRecord
   has_and_belongs_to_many :users
 
   serialize :friends, Array
+  serialize :tags, Array
 
-  def self.scrape_tags
+  def self.scrape_tags # make the user's tag list
+    browser = Watir::Browser.new
+    browser.goto(user.url)
+    # go to their URL and find the headings
+    h1_arr = browser.h1s
+    h3_arr = browser.h3s
+
+    h1_arr.each do |h1|
+      user.tags << h1.text&.strip&.downcase
+    end
+    # scan them in and add them
+    h3_arr.each do |h3|
+      user.tags << h3.text&.strip&.downcase
+    end
+
+    pages = browser.links
+    # find and go to the linked pages on their site
+    pages.each do |page|
+      if page.href.include?(user.url)
+        browser.goto(page.href)
+        # we really only want the links for their site, we don't accidentally want to scan their entire Facebook
+        page_h1_arr.each do |h1|
+          user.tags << h1.text&.strip&.downcase
+        end
+        # same as above - scan and add
+        page_h3_arr.each do |h3|
+          user.tags << h3.text&.strip&.downcase
+        end
+      end
+    end
+
+    end
+
+    user.save
   end
 
 end
