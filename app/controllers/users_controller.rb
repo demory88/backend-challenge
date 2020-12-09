11f8b2ec818957a8f6340fcc69ad11f38974ce84
Don Emory <demory88@gmail.com>
2020-12-08 22:20:02 -0600

    Find expert function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,40 @@ class UsersController < ActionController::Base
     remove = @user.friends << @user.id
     @all_users = User.where.not(id: remove)
 
+    # self-contained search so users don't have to leave their page to find experts
     if params[:search].present?
-      # do search stuff here
+      # find users who match the searched for topics
+      matches = User.where('tags LIKE ?', "%#{params[:topic]&.strip&.downcase}%")
+      @matches = matches
+      @topic = params[:topic]
+
+      # this is six degrees of separation, but without Kevin Bacon
+      @connections = []
+
+      # now we look for our user in a widening circle of friends and connections
+      matches.uniq.each do |match|
+        subpath = []
+        if match.friends.include?(current_user.id)
+          # adding this because, as a user, I'd want to know if an existing friend was an expert in the topic of interest
+          subpath << match
+        else
+          # This would be far cleaner as a recursive function- just calling itself ad infinitum until if found a connection, but I'm up against time and I don't want to cheat
+          match.friends.each do |friend_id|
+            friend = User.find friend_id
+            if friend == current_user
+              subpath << match
+              subpath << friend
+            elsif friend.friends | current_user.friends
+              secondary = User.find (friend.friends | current_user.friends).first
+              subpath << match
+              subpath << friend
+              subpath << secondary
+            end
+          end
+        end
+        subpath << current_user
+        @connections << subpath
+      end
     end
   end
 
@@ -29,7 +61,7 @@ class UsersController < ActionController::Base
 
     friends.each do |friend|
       friend.friends << user.id
-      friend.friends.uniq
+      friend.friends = friend.friends.uniq
       friend.save
     end
     redirect_to :back, notice: "Your friends list is growing!"
